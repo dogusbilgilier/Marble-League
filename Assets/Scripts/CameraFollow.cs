e793bbc75996f5b359abd821d824cd8c0eb7dbb8
dogusbilgilier <dogusbilgilier@gmail.com>
2021-09-04 22:30:37 +0300

    Environment and Voronoi mesh added

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    Vector3 desiredPosition;
+    [Range(0, 1)]
+    public float takipX, takipY;
+    public float offsetY;
+    float x, y, z;
+
+    void FixedUpdate()
+    {
+        desiredPosition = player.position;
+        x = Mathf.Lerp(transform.position.x, desiredPosition.x, takipX);
+        y = Mathf.Lerp(transform.position.y, desiredPosition.y+offsetY, takipY);
+        z = transform.position.z;
+
+        transform.position = new Vector3(x,y,z);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody rb;
+    float speed;
+    Vector3 speedVector;
+    [SerializeField] float defaultSpeed,speedUp,speedMax;
+    void Start()
+    {
+        rb = transform.GetChild(0).GetComponent<Rigidbody>();
+        speed = defaultSpeed;
+        rb.velocity = Vector3.down;
+        OnSpeedChange();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log(rb.velocity);
+
+        rb.velocity = speedVector;
+    }
+    void OnSpeedChange()
+    {
+        
+        speedVector = new Vector3(rb.velocity.x *speed, rb.velocity.y * speed, rb.velocity.z * speed);
+    }
+}
